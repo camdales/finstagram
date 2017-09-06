c28e4789f216d13df1577a4486b8771b1364e65d
Cam Dales <cdales@aritzia.com>
2017-09-06 20:20:07 +0000

    added login and logout functions and redirects to home when logged in and out

--- app/actions.rb
+++ app/actions.rb
@@ -2,6 +2,7 @@
 get '/' do 
     
     @posts = Post.order(created_at: :desc)
+    @current_user = User.find_by(id: session[:user_id])
     erb(:index)
 
 end
@@ -24,9 +25,44 @@ post '/signup' do
     
     #if user validations pass and user is saved
     if @user.save 
-        "User #{username} saved!"
+        redirect to('/login')
     else
         erb(:signup)
     end
     
+end
+
+post '/login' do #when we submit a form with an action of /login
+    username = params[:username]
+    password = params[:password]
+    
+    
+    user=User.find_by(username: username)
+    
+
+        
+        if user && user.password == password 
+            
+            session[:user_id] = user.id
+            redirect to ('/')
+            
+        else
+            @error_message = "total failure"
+            erb(:login)
+        end
+        
+ 
+end
+
+    
+
+get '/login' do #when a GET request comes into  /login
+    erb(:login) #render app/views/login.erb
+    
+end
+    
+    
+get '/logout' do
+    session[:user_id] = nil
+    redirect to ('/')
 end
